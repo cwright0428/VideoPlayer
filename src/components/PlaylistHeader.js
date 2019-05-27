33a83810c7ed2d59442b3b7d0f1fb903de3a01e7
cwright0428 <cwright0428@yahoo.com>
2019-05-27 17:34:44 -0500

    adding playlist components

--- src/components/PlaylistHeader.js
+++ src/components/PlaylistHeader.js
@@ -1,17 +1,16 @@
 import React from "react";
-import Playlist from './containers/Playlist';
-import StyledPlaylistHeader from './styles/StyledPlaylistHeader';
+import Playlist from "./containers/Playlist";
+import StyledPlaylistHeader from "./styles/StyledPlaylistHeader";
+import StyledJourney from "./styles/StyledJourney";
 
-
-
-const PlayListHeader = props => (
-
-   <div>
-        PlaylistHeader!!!
-   </div>
-
-);
-
-
+const PlayListHeader = ({active, total}) =>
+  <StyledPlaylistHeader>
+    <p>
+      {" "}{active.title}{" "}
+    </p>
+    <StyledJourney>
+      {" "}{active.num} / {total}
+    </StyledJourney>
+  </StyledPlaylistHeader>;
 
 export default PlayListHeader;
