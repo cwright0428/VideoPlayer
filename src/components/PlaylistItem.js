b6e1832f9fb5d4f19b35c9fba9a0c20a23572fc1
cwright0428 <cwright0428@yahoo.com>
2019-05-27 17:50:10 -0500

    starting vid player

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -1,15 +1,11 @@
 import React from "react";
-import StyledPlaylistItem from './styles/StyledPlaylistItem';
+import StyledPlaylistItem from "./styles/StyledPlaylistItem";
 
-
-const PlaylistItem = props => (
-
-   <StyledPlaylistItem>
-        PlaylistItem!!!
-   </StyledPlaylistItem>
-
-);
-
-
+const PlaylistItem = ({ video, active, played }) =>
+  <StyledPlaylistItem active={active} played={played}>
+    <div classname="main-player__video-nr"> {video.num} </div>
+    <div classname="main-player__video-name"> {video.title} </div>
+    <div classname="main-player__video-time"> {video.duration} </div>
+  </StyledPlaylistItem>;
 
 export default PlaylistItem;
